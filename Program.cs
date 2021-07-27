9819b64eb785a43e6c23ad666f471c528de183c4
tamilselvansellamuthu <45310733+tamilselvansellamuthu@users.noreply.github.com>
2021-07-27 15:07:20 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,12 +1,37 @@
-﻿using System;
+using System;
+using System.Globalization;
 
 namespace OctocatApp
+// {
+//     class Program
+//     {
+//         static void Main(string[] args)
+//         {
+//             Console.WriteLine("Hello World!");
+//         }
+//     }
+// }
+
+
+
+
+public class Example
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+   public static void Main()
+   {
+      DateTime localDate = DateTime.Now;
+      DateTime utcDate = DateTime.UtcNow;
+      String[] cultureNames = { "en-US", "en-GB", "fr-FR",
+                                "de-DE", "ru-RU" } ;
+
+      foreach (var cultureName in cultureNames) {
+         var culture = new CultureInfo(cultureName);
+         Console.WriteLine("Hello World!");
+         Console.WriteLine("{0}:", culture.NativeName);
+         Console.WriteLine("   Local date and time: {0}, {1:G}",
+                           localDate.ToString(culture), localDate.Kind);
+         Console.WriteLine("   UTC date and time: {0}, {1:G}\n",
+                           utcDate.ToString(culture), utcDate.Kind);
+      }
+   }
 }
